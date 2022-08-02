abd3f3393db8f77a5e1f87eab708e383285352a6
松永玖瑠実 <matsunaga@appart.co.jp>
2022-08-03 01:27:21 +0900

    station03&04 clear

--- app/Http/Controllers/PracticeController.php
+++ app/Http/Controllers/PracticeController.php
@@ -2,22 +2,30 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Practice;
+
 class PracticeController extends Controller
 {
     public function sample()
     {
-        return response('practice');
+        return view('practice');
     }
 
     public function sample2()
     {
         $test = 'practice2';
-        return response($test);
+        return view('practice2', ['testParam' => $test]);
     }
 
     public function sample3()
     {
         $test = 'test';
-        return response($test);
+        return view('practice3', ['testParam' => $test]);
+    }
+
+    public function getPractice()
+    {
+        $practice = Practice::all();
+        return response()->json($practice);
     }
 }
